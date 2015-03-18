b3c25b26b68274def729b741e459532bbcdc2fe4
DanielAndre <danielvitsaus@gmail.com>
2015-03-17 22:19:37 -0300

    Commit Inicial

---
+++ Trabalho_01/Debug/src/Lista.d
@@ -0,0 +1,8 @@
+src/Lista.d: ../src/Lista.cpp ../src/Lista.h ../src/Vertice.h \
+ ../src/Aresta.h
+
+../src/Lista.h:
+
+../src/Vertice.h:
+
+../src/Aresta.h:
---
+++ Trabalho_01/src/Vertice.h
@@ -0,0 +1,9 @@
+#ifndef VERTICE_H_INCLUDED
+#define VERTICE_H_INCLUDED
+#include "Lista.h"
+#include "Aresta.h"
+
+class Vertice;
+
+
+#endif
